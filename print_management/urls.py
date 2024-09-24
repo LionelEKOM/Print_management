f34da9220889707120e9555993b830b08d7ff340
Lionel EKOM <ekomlionel@gmail.com>
2024-09-24 21:52:56 +0100

    "Updated db.sqlite3, added HomePageView, and modified urls.py, base.html, and views.py files"

--- print_management/urls.py
+++ print_management/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from print_management import settings
+from printers.views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageView.as_view()),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
